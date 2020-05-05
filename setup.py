62066c66dc799b048518b2e9bd1120d0226dc28b
Oscar <oscar.s.kremer@hotmail.com>
2020-05-05 17:26:38 -0300

    add anomaly-detection

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Code for anomaly detection, testing with numenta and its benchmark datasets',
+    author='Oscar Schmitt Kremer',
+    license='MIT',
+)
